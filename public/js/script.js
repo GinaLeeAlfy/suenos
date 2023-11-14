3160045a15b5c55d88667a516cc110dc730cc204
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-11-13 18:05:18 -0800

    script can spawn wishes inside wish container based on size of container

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,23 @@
 const wishContainer = document.querySelector('.wish-container');
 
-const FOOTER_HEIGHT = 220;
 const TOOLTIP_WIDTH = 293;
 let totalWishes;
 const wishesDisplayed = [];
 
 function calcQuantityWishes() {
+    let wishContainerHeight = wishContainer.offsetHeight;
+    let wishContainerWidth = wishContainer.offsetWidth;
     totalWishes = Math.round(
-        (((window.innerHeight - FOOTER_HEIGHT) * window.innerWidth) / 1600) *
-            0.1
+        ((wishContainerHeight * wishContainerWidth) / 1600) * 0.1
     );
+    return totalWishes;
 }
 
 function createWishEl() {
+    //create wish element
+    // wishContainer = document.querySelector('.wish-container');
+    let wishContainerHeight = wishContainer.offsetHeight;
+    let wishContainerWidth = wishContainer.offsetWidth;
     const wishStar = document.createElement('div');
     const tooltip = document.createElement('div');
     const wishContent = document.createElement('div');
@@ -26,47 +31,59 @@ function createWishEl() {
     tooltip.append(wishLike);
     wishStar.append(tooltip);
 
-    let topLocation = calcLocation(window.innerHeight - FOOTER_HEIGHT);
-    let leftLocation = calcLocation(window.innerWidth);
+    let topLocation = calcLocation(wishContainerHeight);
+    let leftLocation = calcLocation(wishContainerWidth);
 
     wishStar.style.top = `${topLocation}px`;
     wishStar.style.left = `${leftLocation}px`;
 
-    let halfWishContainerHeight = (window.innerHeight - FOOTER_HEIGHT) / 2;
-    let halfWishContainerWidth = window.innerWidth / 2;
+    let halfWishContainerHeight = wishContainerHeight / 2;
+    let halfWishContainerWidth = wishContainerWidth / 2;
 
+    //position tooltip vertically
     if (topLocation >= halfWishContainerHeight) {
         tooltip.classList.add('tooltip-bottom');
     } else {
         tooltip.classList.add('tooltip-top');
     }
 
-    console.log(wishStar.style.left);
-    console.log(halfWishContainerWidth);
-
+    //position tooltip horizontally
     if (leftLocation >= halfWishContainerWidth) {
         tooltip.classList.add('tooltip-right');
     } else {
         tooltip.classList.add('tooltip-left');
     }
 
-    if (window.innerWidth <= TOOLTIP_WIDTH * 2) {
+    //if screen is too small position tooltip horizontal differently
+    if (wishContainerWidth <= TOOLTIP_WIDTH * 2) {
+        if (leftLocation >= halfWishContainerWidth) {
+            tooltip.style.right = `${
+                (wishContainerWidth - leftLocation) * -1 + 15
+            }px`;
+        } else {
+            tooltip.style.left = `${leftLocation * -1 + 7}px`;
+        }
         tooltip.classList.remove('tooltip-right', 'tooltip-left');
-        tooltip.style.left = `${leftLocation * -1 + 7}px`;
     }
 
     wishContainer.append(wishStar);
+
+    const wishStarObject = {
+        wishStar: wishStar,
+        top: topLocation,
+        left: leftLocation,
+    };
+
+    return wishStarObject;
 }
 
 function calcLocation(length) {
-    let randomSpot = Math.round((length * Math.random()) / 40) * 40;
+    let randomSpot = Math.floor((length - 40) * Math.random()) + 15;
 
-    if (randomSpot < length - 88) {
-        randomSpot = `${randomSpot + 16}`;
-    } else {
-        randomSpot = `${randomSpot - 64}`;
-    }
     return randomSpot;
 }
 
-createWishEl();
+calcQuantityWishes();
+for (let index = 0; index < totalWishes; index++) {
+    createWishEl();
+}
