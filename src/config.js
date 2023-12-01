8a6db4a2045dca6e196695df5186301698a31286
Jesus Magana <maganazero5@gmail.com>
2023-11-30 20:05:44 -0800

    Add logic to reject invalid updates and limit get request
    Get Wishes now takes in a query parameter.
    patch request not rejects invalid updates

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
-import { config } from 'dotenv'
-import pg from 'pg'
+import { config } from 'dotenv';
+import pg from 'pg';
 
 const envFile =
     process.env.NODE_ENV === 'production'
         ? '.env.production'
-        : '.env.development'
-config({ path: envFile })
+        : '.env.development';
+config({ path: envFile });
 
 const pool = new pg.Pool({
     user: process.env.DB_USER,
@@ -13,6 +13,6 @@ const pool = new pg.Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-})
+});
 
-export default pool
+export default pool;
